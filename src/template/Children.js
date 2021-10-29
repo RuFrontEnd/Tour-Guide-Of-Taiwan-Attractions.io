ecde1d5ab1181ae825c978684c7761f486edef39
RuFrontEnd <k718198@gmail.com>
2021-10-29 09:07:18 +0800

    add style attribute of Templates

--- src/template/Children.js
+++ src/template/Children.js
@@ -2,8 +2,12 @@ import React from "react";
 import styled from "styled-components/macro";
 
 const Children = (props) => {
-  const { className, children } = props;
-  return <Box className={className}>{children}</Box>;
+  const { className, style, children } = props;
+  return (
+    <Box style={style} className={className}>
+      {children}
+    </Box>
+  );
 };
 
 const Box = styled.section``;
--- src/template/Template.js
+++ src/template/Template.js
@@ -2,8 +2,8 @@ import React from "react";
 import styled from "styled-components/macro";
 
 const Template = (props) => {
-  const { className } = props;
-  return <Box className={className}></Box>;
+  const { className, style } = props;
+  return <Box style={style} className={className}></Box>;
 };
 
 const Box = styled.section``;
