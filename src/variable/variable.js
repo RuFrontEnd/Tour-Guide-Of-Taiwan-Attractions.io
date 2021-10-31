c957933bb2ea86cbcb14ad9d3e86e800a0f1a133
RuFrontEnd <k718198@gmail.com>
2021-10-31 14:09:19 +0800

    globally change font family to notos

--- src/variable/variable.js
+++ src/variable/variable.js
@@ -12,13 +12,12 @@ export const selectedShadowColor = "0px 2px 5px #C0C7D0;";
 export const seperateBarColor = "#F1F2F2";
 export const optionButtonColor = "#646466";
 export const blueGreen = "#38D0B7";
-export const notoSans = '"Noto Sans", sans-serif';
+export const notoSans = "'Noto Sans TC', sans-serif";
+export const roboto = "'Roboto', sans-serif";
 
 export const __0D0B0C__ = (opacity) =>
   `${opacity ? `rgba(13, 11, 12, ${opacity})` : "#0D0B0C"}`;
 export const __D2D2D2__ = (opacity) =>
   `${opacity ? `rgba(210, 210, 210, ${opacity})` : "#D2D2D2"}`;
-  export const __FF1D6C__ = (opacity) =>
+export const __FF1D6C__ = (opacity) =>
   `${opacity ? `rgba(255, 29, 108, ${opacity})` : "#FF1D6C"}`;
-
-
