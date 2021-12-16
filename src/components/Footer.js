a602a79643f5d147e192f2cbd144d61287fdcb92
RuFrontEnd <k718198@gmail.com>
2021-12-16 20:45:28 +0800

    修改Footer樣式

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,45 @@
 import React from "react";
 import styled from "styled-components/macro";
 import { __FFF__ } from "variable/variable";
+import Space from "layouts/Space";
 
 const Footer = (props) => {
   const { className, style } = props;
   return (
-    <Box style={style} className={className}>
-      Taiwan Tourguide © Code: Luke_Xie / Design: KT
-    </Box>
+    <Container style={style} className={className}>
+      <Wrap>
+        <Box>
+          <Text>Taiwan Tourguide ©</Text>
+          <Text>Code: Luke_Xie</Text>
+          <Text>Design: KT</Text>
+        </Box>
+      </Wrap>
+    </Container>
   );
 };
 
-const Box = styled.section`
+const Text = styled.p`
+  padding: 0px 2px;
+  @media (max-width: 425px) {
+    padding: 4px 2px;
+  }
+`;
+
+const Box = styled.div`
   display: flex;
   justify-content: center;
-  background-color: ${__FFF__()};
+  text-align: center;
   padding: 25.5px 0px;
+
+  @media (max-width: 425px) {
+    flex-direction: column;
+  }
+`;
+
+const Wrap = styled(Space)``;
+
+const Container = styled.section`
+  background-color: ${__FFF__()};
 `;
 
 export default Footer;
